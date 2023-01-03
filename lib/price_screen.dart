b823e8d2f2bdd0b6eb6705f610fb661d60f8b834
abayomiDare <01.abayomidare>
2023-01-03 20:16:11 +0100

    Initial commit

---
+++ lib/price_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'coin_data.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  late String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropdownCurrencyList() {
+    List<DropdownMenuItem<String>> menuItem = [];
+
+    for (String currency in currenciesList) {
+      var currencyVal = DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
+      menuItem.add(currencyVal);
+    }
+    return menuItem;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 100.0,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: getDropdownCurrencyList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value!;
+                });
+                print(value);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
